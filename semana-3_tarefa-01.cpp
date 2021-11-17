6a5bade4408bc27e18a0f6105fd3d4f2c9755f9a
Paio <caio_g_franco@outlook.com>
2021-11-17 15:47:14 -0300

    Tarefa 01 added

---
+++ semana-3_tarefa-01.cpp
@@ -0,0 +1,39 @@
+#include "Image.h"
+
+int main(){
+	
+    Image img(600, 600);
+
+    // Se imagem é 600 x 600 então cada quadrado tem 75 x 75
+            
+	img.fill(white);
+
+    bool paintItBlack = false;
+
+    int count_x = 0, count_y = 0;
+
+	for(int y = 0; y < 600; y++){
+		
+        if(count_y > 75){
+            paintItBlack = !paintItBlack;
+            count_y = 0;
+        }
+        
+        for(int x = 0; x < 600; x++){
+
+            if(count_x > 75){
+                paintItBlack = !paintItBlack;
+                count_x = 0;
+            }
+
+            if (paintItBlack)
+                img(x,y) = black;
+
+            count_x++;
+        }
+        count_y++;
+    }
+	img.savePNG("output_S2-1.png");
+
+    return 0; 
+}
