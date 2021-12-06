79410faa2f809b13017f8de0776bd0993f9f7fd1
Paio <caio_g_franco@outlook.com>
2021-12-06 19:35:36 -0300

    Added draw primitive

--- Graphics2D.h
+++ Graphics2D.h
@@ -56,6 +56,16 @@ class Graphics2D{
 
 	void drawPrimitive(Triangle<Vec2Col> tri){
 	/**************** TAREFA - AULA 09 **************/
+		vec2 v0 = tri[0].position, v1 = tri[1].position, v2 = tri[2].position;
+		Color c0 = tri[0].color, c1 = tri[1].color, c2 = tri[2].color;
+
+        std::array<vec2, 3> pontos = {v0,v1,v2};
+        for(Pixel p: rasterizeTriangle(pontos)){
+            vec2 v = {(float)p.x, (float)p.y};
+			vec3 resultado = barycentric(v, pontos);
+			Color color = toColor((resultado[0]*toVec3(c0)) + (resultado[1]*toVec3(c1)) + (resultado[2]*toVec3(c2)));
+			paint(p, color);
+		}
 	}
 };
 
